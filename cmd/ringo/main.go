792acc1090df5d13ebf7e929a4ed1e2738e14460
Masaya Hayashi <mcdonalds.only@gmail.com>
2022-10-30 20:34:57 +0900

    update

--- cmd/ringo/main.go
+++ cmd/ringo/main.go
@@ -22,7 +22,13 @@ func run() (code int) {
 	defer stop()
 
 	r := chi.NewRouter()
+
 	r.Use(middleware.Logger)
+	r.Use(func(next http.Handler) http.Handler {
+		return http.MaxBytesHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+		}), 4096)
+	})
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(map[string]interface{}{
