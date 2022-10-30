81d5b984f317bda2f04bcd2910c071eb37631369
Masaya Hayashi <mcdonalds.only@gmail.com>
2022-10-30 20:32:57 +0900

    update

--- cmd/ringo/main.go
+++ cmd/ringo/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	os.Exit(run())
+}
+
+func run() (code int) {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
@@ -18,5 +30,30 @@ func main() {
 		})
 	})
 
-	http.ListenAndServe(":8080", r)
+	server := http.Server{
+		Addr:              ":8080",
+		Handler:           r,
+		ReadTimeout:       5 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	go func() {
+		<-ctx.Done()
+		ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			code = 1
+		}
+	}()
+
+	if err := server.ListenAndServe(); err != nil {
+		switch err {
+		case http.ErrServerClosed:
+		default:
+			return 1
+		}
+	}
+
+	return 0
 }
